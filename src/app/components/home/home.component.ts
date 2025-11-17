de739c1155bf7ae012f5a4feb84916eddab1ecc9
Pedro Lucas <pedrolucasmoraisborges@gmail.com>
2025-11-17 13:08:03 -0300

    feat(home_first_gets)

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,26 +1,112 @@
 import { Component, inject, Inject } from '@angular/core';
-import { EnviaFormularioService } from '../../services/envia-formulario.service';
 import { CommonModule } from '@angular/common';
+import { EnviorementsService } from '../../services/enviorements.service';
+import { FormsModule } from '@angular/forms';
+import { BuildingsService } from '../../services/buildings.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  private enviaFormularioService = inject(EnviaFormularioService);
+  private enviorementsService = inject(EnviorementsService);
+  private buildingsService = inject(BuildingsService);
+  private toastr = inject(ToastrService);
+
   reservedEnviorements: number = 0;
   canceledReserves: number = 0;
   activeEnviorements: number = 0;
-  rooms: any[] = [];
+
+  enviorements: any[] = [];
+  buildings: any[] = [];
+  selectedEnviorement: any = null;
+  editMode: boolean = false;
   addNewEnviorement: boolean = false;
 
-  submit(event: any){
-    this.enviaFormularioService.enviaInfoBackend(this.rooms);
-  }
 
   handleAddNewEnviorement(){
     this.addNewEnviorement = !this.addNewEnviorement;
+    this.selectedEnviorement = {};
+  }
+
+  handleEditEnviorement(item: any) {
+    this.selectedEnviorement = { ...item };
+    this.editMode = true;
+    this.addNewEnviorement = true;
+  }
+
+  closeModal() {
+    this.addNewEnviorement = false;
+    this.editMode = false;
+    this.selectedEnviorement = null;
+  }
+
+  reloadEnvs() {
+  this.enviorementsService.getEnviorements().subscribe({
+    next: (res) => {
+      this.enviorements = res.environment || [];
+    }
+  });
+}
+
+  deleteEnviorement(id: string) {
+    this.enviorementsService.deleteEnviorement(id).subscribe({
+      next: () => {
+        this.reloadEnvs();
+      }
+    })
+  }
+  saveEnviorement() {
+    const payload = {
+      name: this.selectedEnviorement.name,
+      fk_build: this.selectedEnviorement.fk_build,
+      floor: this.selectedEnviorement.floor,
+      description: this.selectedEnviorement.description
+    };
+
+    if (this.editMode) {
+      this.enviorementsService.updateEnviorement(this.selectedEnviorement.id, payload)
+        .subscribe({
+          next: () => {
+            this.closeModal();
+            this.reloadEnvs();
+            // this.toastr.success('Ambiente removido com sucesso');
+          },
+          error: () => console.log('Erro ao atualizar ambiente')
+        });
+    } else {
+        this.enviorementsService.createEnviorement(payload)
+          .subscribe({
+            next: () => {
+              this.closeModal();
+              this.reloadEnvs();
+            },
+            error: () => console.log('Erro ao criar ambiente')
+          });
+      }
+  }
+
+  ngOnInit() {
+    this.buildingsService.getAllBuildings().subscribe({
+      next: (res) => {
+        this.buildings = res.buildings || [];
+      },
+      error: () => {
+        console.log('Erro ao carregar os prédios');
+      }
+    });
+
+    this.enviorementsService.getEnviorements().subscribe({
+      next: (res) => {
+        this.enviorements = res.environment;
+        console.log('Ambientes carregados:', this.enviorements);
+      },
+      error: () => {
+        console.log('Erro ao carregar os ambientes');
+      }
+    });
   }
 }
--- src/app/services/enviorements.service.ts
+++ src/app/services/enviorements.service.ts
@@ -6,6 +6,34 @@ import { inject, Injectable } from '@angular/core';
 })
 export class EnviorementsService {
   private http = inject(HttpClient)
-  private baseUrl = 'http://localhost:8080/auth';
+  private baseUrl = 'http://localhost:8080/environments';
+  private token? = localStorage.getItem('token') || '';
 
+  getEnviorements() {
+    const headers = {
+      Authorization: `Bearer ${this.token}`
+    };
+    return this.http.get<any>(`${this.baseUrl}/getAllenvironments`, {headers});
+  }
+
+  createEnviorement(payload: any) {
+    const headers = {
+      Authorization: `Bearer ${this.token}`
+    };
+    return this.http.post<any>(`${this.baseUrl}/createenvironment`, payload, {headers});
+  }
+
+  updateEnviorement(id: string, payload: any) {
+    const headers = {
+      Authorization: `Bearer ${this.token}`
+    };
+    return this.http.put<any>(`${this.baseUrl}/updateenvironment/${id}`, payload, {headers});
+  }
+
+  deleteEnviorement(id: string) {
+    const headers = {
+      Authorization: `Bearer ${this.token}`
+    };
+    return this.http.delete<any>(`${this.baseUrl}/deleteenvironment/${id}`, {headers});
+  }
 }
